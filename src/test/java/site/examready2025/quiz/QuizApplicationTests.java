ef79ae65342c427a24b982e83bf80350e14bfffb
HyominAn0401 <slhm7547@naver.com>
2024-11-26 00:31:55 +0900

    Initial commit

---
+++ src/test/java/site/examready2025/quiz/QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package site.examready2025.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
