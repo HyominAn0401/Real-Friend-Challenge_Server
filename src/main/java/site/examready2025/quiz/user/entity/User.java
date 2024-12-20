1e602093ccdc4dcf9bdf3fd71bff94d541cdddb2
HyominAn0401 <slhm7547@naver.com>
2024-12-20 12:50:42 +0900

    Feat: User entity 추가

--- src/main/java/site/examready2025/quiz/user/entity/User.java
+++ src/main/java/site/examready2025/quiz/user/entity/User.java
@@ -1,4 +1,25 @@
 package site.examready2025.quiz.user.entity;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
 }
