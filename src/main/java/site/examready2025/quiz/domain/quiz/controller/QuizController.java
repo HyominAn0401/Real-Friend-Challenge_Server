60e65eedc672d01097aa514008eac892f6bc2f7a
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2024-12-21 01:17:27 +0900

    feat: QuizResponseDto 추가

--- src/main/java/site/examready2025/quiz/domain/quiz/controller/QuizController.java
+++ src/main/java/site/examready2025/quiz/domain/quiz/controller/QuizController.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.examready2025.quiz.domain.quiz.Dto.QuizRequestDto;
+import site.examready2025.quiz.domain.quiz.Dto.QuizResponseDto;
 import site.examready2025.quiz.domain.quiz.entity.Quiz;
 import site.examready2025.quiz.domain.quiz.service.QuizService;
 
@@ -16,8 +16,13 @@ public class QuizController {
     private final QuizService quizService;
 
     @PostMapping("/api/quizzes")
-    public ResponseEntity<String> createQuiz(@RequestBody QuizRequestDto requestDto) {
+    public ResponseEntity<QuizResponseDto> createQuiz(@RequestBody QuizRequestDto requestDto) {
+        // Quiz 생성
         Quiz quiz = quizService.createQuiz(requestDto);
-        return ResponseEntity.ok("퀴즈가 생성되었습니다.");
+
+        // QuizResponseDto 생성
+        QuizResponseDto responseDto = new QuizResponseDto(quiz.getTitle());
+
+        return ResponseEntity.ok(responseDto);
     }
 }
