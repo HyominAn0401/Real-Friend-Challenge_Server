756f4e65b81c13298a06b4ada5232759f6106cf1
HyominAn0401 <slhm7547@naver.com>
2025-01-02 23:39:23 +0900

    Fix: responseDto

--- src/main/java/site/examready2025/quiz/domain/response/dto/ResponseDto.java
+++ src/main/java/site/examready2025/quiz/domain/response/dto/ResponseDto.java
@@ -7,14 +7,14 @@ import site.examready2025.quiz.domain.response.entity.Response;
 public class ResponseDto {
 
     private Long id;
-    private Long quizId;
+    //private Long quizId;
     private Long responseUserId;
     private int score;
     private boolean isCompleted;
 
     public ResponseDto(Response response){
         this.id = response.getId();
-        this.quizId = response.getQuiz().getId();
+        //this.quizId = response.getQuiz().getId();
         this.responseUserId = response.getResponseUser().getId();
         this.score = response.getScore();
         this.isCompleted = response.isCompleted();
